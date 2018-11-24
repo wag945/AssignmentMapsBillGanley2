8b1dcf0f4693c44f5020186aef44ad915e60033c
williamganley <william.ganley@missionse.com>
2018-11-24 16:11:22 -0500

    Remove unneeded imports

--- app/src/main/java/com/example/bill/assignment_maps_billganley/MapActivity.java
+++ app/src/main/java/com/example/bill/assignment_maps_billganley/MapActivity.java
@@ -3,10 +3,8 @@ package com.example.bill.assignment_maps_billganley;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -20,8 +18,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
